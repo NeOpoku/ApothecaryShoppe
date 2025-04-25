48b81d34a67542a36df496afdbacde8d76a6cda5
Diana Gaston <Bahashem@yahoo.com>
2025-04-25 08:45:54 -0400

    added client files and code

---
+++ Main/client/src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ApolloProvider from "../utils/ApolloProvider";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Navbar from "./components/navbar";
+import SavedSearches from "./pages/SavedSearches";
+import CustomRecipe from "./pages/CustomRecipe";
+import "./App.css";
+
+function App() {
+  return (
+    <ApolloProvider>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/saved" element={<SavedSearches />} />
+            <Route path="create-recipe" element={<CustomRecipe />}/>
+            <Route path="search" element={<HerbSearch/>}/>
+            <Route path="/recipes" element={<BrowseRecipies/>}/>
+            <Route path="/about" element={<AboutPage/>}/>
+
+          </Routes>
+          {Auth.loggedIn() && GET_HERBS.length > 0 && (
+            <button
+              onClick={() =>
+                SavedSearches({
+                  variables: {
+                    query,
+                    results: GET_HERBS.map((h) => h._id),
+                  },
+                })
+              }
+            >
+              Save This Search
+            </button>
+          )}
+        </div>
+      </BrowserRouter>
+    </ApolloProvider>
+  );
+}
+export default App;
+// This code sets up a basic React application with Apollo Client for GraphQL queries and mutations. It includes routes for the home page, login, and registration, as well as a navigation bar. The application is styled with CSS and uses React Router for client-side routing.
---
+++ Main/client/src/pages/Login.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import {useMutation} from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '..utils/auth';
+
+export default function Login(props) {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [login, { error }] = useMutation(LOGIN_USER);
+
+    const handleSubmit = async(e) => {e.preventDefault();
+        const { data } = await login({variables: { ...formState },
+            });
+            Auth.login(data.login.token);
+        } catch (e) {
+            console.error(e);
+        };
+        return(
+
+        )
+    };
+
+    return (
+      <form onSubmit={handleSubmit }>
+        <inputtype="email"
+        placeholder="Email"
+        value={formState.email}
+        onChange={(e) => setFormState({ ...formState, email: e.target.value })}
+        />
+        <input type="password"
+        placeholder="Password"
+        value={formState.password}
+        onChange={(e) => setFormState({ ...formState, password: e.target.value })}
+        />
+        <button type="submit">Login</button>
+        {error && <div>Login failed</div>}
+        </form>
+    );
+}
