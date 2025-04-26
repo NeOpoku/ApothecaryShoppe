7d6c4d6ccaaeaf8b96034a0a6232e1c3abb10375
NeOpoku <imanimoss17@gmail.com>
2025-04-26 15:31:53 -0400

    added styling to webpage

---
+++ src/Components.tsx/Footer.tsx
@@ -0,0 +1,9 @@
+// src/components/layout/Footer.tsx
+export default function Footer() {
+    return (
+      <footer className="bg-softBrown text-cream p-4 text-center rounded-t-2xl">
+        <p className="font-japanese text-sm">© 2025 The Apothecary Shoppe</p>
+      </footer>
+    );
+  }
+  
