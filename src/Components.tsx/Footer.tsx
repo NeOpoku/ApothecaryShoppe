7c04a13fc61beb701cb6ac60ff95bb9c43ec3bd2
NeOpoku <imanimoss17@gmail.com>
2025-04-28 10:43:02 -0400

    added components

--- src/Components.tsx/Footer.tsx
+++
@@ -1,9 +0,0 @@
-// src/components/layout/Footer.tsx
-export default function Footer() {
-    return (
-      <footer className="bg-softBrown text-cream p-4 text-center rounded-t-2xl">
-        <p className="font-japanese text-sm">© 2025 The Apothecary Shoppe</p>
-      </footer>
-    );
-  }
-  
