805fa05fe85242c1090db4b3075d0a0285c55864
NeOpoku <imanimoss17@gmail.com>
2025-05-04 19:52:16 -0400

    made changes

--- Server/Routes/userRoutes.ts
+++ Server/Routes/userRoutes.ts
@@ -1,6 +1,12 @@
 import express from 'express';
+<<<<<<< Updated upstream
 import { getUser } from '../Controllers/usercontroller';
+=======
+import { getUser } from '../Controllers/herbController';
+>>>>>>> Stashed changes
 
 const router = express.Router();
 
 router.get('/user/:id', getUser);
+
+export default router;
