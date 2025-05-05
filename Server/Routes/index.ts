eb6ffd2964404adb4dca0074088fb23f6aaab0bc
Diana Gaston <Bahashem@yahoo.com>
2025-05-05 19:23:18 -0400

    removed duplicate files (#29)

--- Server/Routes/index.ts
+++ Server/Routes/index.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import authRoutes from './Routes/API/auth-routes.ts';
-import apiRoutes from './API/index.js';
+import apiRoutes from './API/index.ts';
 import { authenticateToken } from '../Middleware/Auth.ts';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
-// TODO: Add authentication to the API routes
-router.use('/api', apiRoutes);
 
 export default router;
